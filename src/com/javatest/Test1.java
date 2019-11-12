09253809c94a28c46b28f96eb9d163ab647ab3ca
gunjan <gmamd21@gmail.com>
2019-11-12 23:22:27 +0530

    commiting the stash{0} code from the master branch

--- src/com/javatest/Test1.java
+++ src/com/javatest/Test1.java
@@ -5,6 +5,9 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Gunjan made changes second");
 		System.out.println("Gunjan made third changes!");
+		System.out.println("Gunjan made new enhancements, added code for forgot password to use the stash command");
+		
+		System.out.println("Changes made after the file was stashed and then stored in the stash directory, code is not committed yet... ");
 	}
 
 }
